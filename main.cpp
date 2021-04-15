edd6846d9434a2cac9ee4145a433327d4ee3bda2
Danyil Somin <danyil.somin@gmail.com>
2021-04-15 10:43:54 +0300

    init

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include <unordered_set>
+
+int main()
+{
+    std::unordered_set<std::string> words{  };
+
+    do {
+        std::string tmpStr;
+        std::cin >> tmpStr;
+        words->insert(tmpStr);
+    }
+    while (std::cin.peek() == ' ');
+
+    std::cout << words.size() << '\n';
+}
